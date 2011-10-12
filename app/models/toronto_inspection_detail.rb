264954d78e9bd3bc6158d4a204c883804e370d5a
NatalieBlack <natalievictoriablack@gmail.com>
2011-10-12 13:57:35 -0400

    validations for out of business establishments

--- app/models/toronto_inspection_detail.rb
+++ app/models/toronto_inspection_detail.rb
@@ -13,19 +13,23 @@ class TorontoInspectionDetail
 
   validates_presence_of :dinesafe_id, :status
 
-  validates_inclusion_of :status, :in => ['Closed', 'Pass', 'Conditional Pass']                                      
+  validates_inclusion_of :status, :in => ['Closed', 'Pass', 'Conditional Pass', 'Out of Business']                                      
 
-  validates_presence_of :severity, :description, :action, :unless => :pass?
+  validates_presence_of :severity, :description, :action, :unless => Proc.new {|c| c.pass? || c.out_of_business?} 
 
   validate :uniqueness_of_dinesafe_id
 
-  validates_inclusion_of :action, :in => ['Notice to Comply', 'Not in Compliance', 'Corrected During Inspection', 'Ticket', 'Summons', 'Order', 'Closure Order', 'Summons and Health Hazard Order'], :unless => :pass?                                  
-  validates_inclusion_of :severity, :in => ['C - Crucial', 'S - Significant', 'M - Minor', 'NA - Not Applicable'], :unless => :pass?                                       
+  validates_inclusion_of :action, :in => ['Notice to Comply', 'Not in Compliance', 'Corrected During Inspection', 'Ticket', 'Summons', 'Order', 'Closure Order', 'Summons and Health Hazard Order'], :unless => Proc.new {|c| c.pass? || c.out_of_business?} 
+  validates_inclusion_of :severity, :in => ['C - Crucial', 'S - Significant', 'M - Minor', 'NA - Not Applicable'], :unless => Proc.new {|c| c.pass? || c.out_of_business?} 
 
-  validates_inclusion_of :court_outcome, :in => ['Pending', 'Conviction - Suspended Sentence', 'Conviction - Ordered to Close by Court', 'Conviction - Fined', 'Charges Withdrawn', 'Charges Quashed'], :unless => :pass?, :allow_blank => true
+  validates_inclusion_of :court_outcome, :in => ['Pending', 'Conviction - Suspended Sentence', 'Conviction - Ordered to Close by Court', 'Conviction - Fined', 'Charges Withdrawn', 'Charges Quashed'], :unless => Proc.new {|c| c.pass? || c.out_of_business?}, :allow_blank => true
 
   def pass?
     status == 'Pass'
+  end
+
+  def out_of_business?
+    status == 'Out of Business'
   end
 
 private
